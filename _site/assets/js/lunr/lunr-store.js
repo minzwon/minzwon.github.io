222b23addb13c4b2fa6b5aa2399ae70592f2d363
Minz Won <minz.won@upf.edu>
2018-05-11 02:18:18 +0200

    init

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "WWW 2018 Challenge: Learning to Recognize Musical Genre",
+        "excerpt":"I won the “WWW 2018 Challenge: Learning to Recognize Musical Genre” with Jay Kim. This post describes our approach for the challenge. Full paper is available here. Final result Intro About challenge This year, the Web conference, also known as WWW, newly organized a challenge track. Learning to Recognize Musical...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/research/",
+        "teaser":"http://localhost:4000/assets/images/profile.jpg"}]
