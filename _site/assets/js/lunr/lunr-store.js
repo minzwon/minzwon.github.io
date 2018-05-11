ea1b6122e81af2e097dc62f87c30c46b7f3c0cfb
Minz Won <minz.won@upf.edu>
2018-05-11 14:56:36 +0200

    dl4mir

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,6 +4,11 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/research/2017-09-11-benvinguda/",
         "teaser":"http://localhost:4000/assets/images/profile.jpg"},{
+        "title": "dl4mir docker image",
+        "excerpt":"dl4mir is a docker image for the deep learning for music information retrieval research. (Github) Contents For music analysis: essentia, librosa, madmom For deep learning: pytorch, keras, tensorflow, sklearn, cuda and some useful python libraries including: numpy, scipy, pandas, matplotlib, seaborn, jupyter and more! Versions dl4mir:gpu-py2 python2 environment dl4mir:gpu-py3 python3...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/research/2018-04-21-dl4mir/",
+        "teaser":"http://localhost:4000/assets/images/profile.jpg"},{
         "title": "WWW 2018 Challenge: Learning to Recognize Musical Genre",
         "excerpt":"I won the “WWW 2018 Challenge: Learning to Recognize Musical Genre” with Jay Kim. This post describes our approach for the challenge. Full paper is available here. Final result (link) Intro About challenge This year, the Web conference, also known as WWW, newly organized a challenge track. Learning to Recognize...","categories": [],
         "tags": [],
